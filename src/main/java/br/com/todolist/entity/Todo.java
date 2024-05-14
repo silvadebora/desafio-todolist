9cfb61bea44050d6f2eef66493d87049bab81199
Debora <silvadeborafisio@gmail.com>
2024-05-14 17:32:11 -0300

    feat: created todo class

---
+++ src/main/java/br/com/todolist/entity/Todo.java
@@ -0,0 +1,73 @@
+package br.com.todolist.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "todos")
+public class Todo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String description;
+
+    private boolean isDone;
+
+    private int priority;
+
+    public Todo() {
+    }
+
+    public Todo(Long id, String name, String description, boolean isDone, int priority) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.isDone = isDone;
+        this.priority = priority;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isDone() {
+        return isDone;
+    }
+
+    public void setDone(boolean done) {
+        isDone = done;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
+}
